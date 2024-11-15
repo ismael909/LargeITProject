1b79c35795fd834d2d25ccc5d2a9177811d3d7e0
AchilleasLeiv <a.leivadiotis@ng-sensors.com>
2024-11-15 18:10:00 +0100

    Added clothes to the repository so we dont have to add them directly

--- live-demo-main/src/main/java/lsit/Repositories/ClothesRepository.java
+++ live-demo-main/src/main/java/lsit/Repositories/ClothesRepository.java
@@ -1,15 +1,32 @@
 package lsit.Repositories;
 
+
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
+import javax.annotation.PostConstruct;
 
 import org.springframework.stereotype.Repository;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lsit.Models.Clothes;
 
 @Repository
 public class ClothesRepository {
     static HashMap<UUID, Clothes> clothes = new HashMap<>();
-
+    @PostConstruct
+    public void initData() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            List<Clothes> clothesList = objectMapper.readValue(new File("clothes.json"),
+                    new TypeReference<List<Clothes>>() {});
+            clothesList.forEach(clothesItem -> clothes.put(clothesItem.id, clothesItem));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void add(Clothes p){
         p.id = UUID.randomUUID();
         clothes.put(p.id, p);
@@ -23,10 +40,13 @@ public class ClothesRepository {
         clothes.remove(id);
     }
 
-    public void update(Clothes p){
+    public void update(Clothes p) {
         Clothes x = clothes.get(p.id);
-        x.name = p.name;
-        x.kind = p.kind;
+        if (x != null) {
+            x.name = p.name;
+            x.kind = p.kind;
+            x.size = p.size;
+        }
     }
 
     public List<Clothes> list(){
